94ff74d7b417fa734f3404b8a4c3ac5908a58f08
Rafal Galka <rafal@modernweb.pl>
2012-06-12 16:00:35 +0200

    paginator partial

---
+++ views/scripts/partial/paginator-search.php
@@ -0,0 +1,35 @@
+<?php /* @see http://developer.yahoo.com/ypatterns/pattern.php?pattern=searchpagination */ ?>
+<div class="pagination"><ul>
+
+    <?php $class = (isset($this->previous)) ? '' : 'disabled' ?>
+    <?php $href = (isset($this->previous))
+        ? $this->url(array('page' => $this->previous), 'blog', false, false)
+        : '' ?>
+    <li class="<?=$class?>">
+        <a href="<?=$href?>">
+            &laquo; <?=$this->translate('Previous')?>
+        </a>
+    </li>
+
+    <?php foreach ($this->pagesInRange as $page): ?>
+        <?php $class = ($page == $this->current) ? 'active' : '' ?>
+        <?php $href = ($page == $this->current)
+            ? ''
+            : $this->url(array('page' => $page), 'blog', false, false) ?>
+        <li class="<?=$class?>">
+            <a href="<?=$href?>">
+                <?=$page?>
+            </a>
+        </li>
+    <?php endforeach; ?>
+
+    <?php $class = (isset($this->next)) ? '' : 'disabled' ?>
+    <?php $href = (isset($this->next))
+        ? $this->url(array('page' => $this->next), 'blog', false, false)
+        : '' ?>
+    <li class="<?=$class?>">
+        <a href="<?=$href?>">
+            <?=$this->translate('Next')?> &raquo;
+        </a>
+    </li>
+</ul></div>
